bf8b2eb38dc2e722446a1e8d0c0ad2d4909de741
sabgirl <sbartoli@alumni.escaet.fr>
2020-07-23 17:39:10 +0200

    begin formulaire

--- src/components/nousRejoindre/NousRejoindre.js
+++ src/components/nousRejoindre/NousRejoindre.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 
-function NousRejoindre() {
+const NousRejoindre = () => {
+    const [inscription, setInscription] = useState([
+        lastname= "",
+        firstname= "",
+        addressPraloup= "",
+        address="",
+        tel = "",
+        email = ""
+    ])
+
+    
     return (
         <div>
             <p>test</p>
